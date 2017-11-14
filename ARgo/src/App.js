f6f3d29d50866e45cb2bd6a0b2367d84d16226b9
Grace Park <gracepark@github.com>
2017-11-14 14:44:12 -0800

    initial files

---
+++ ARgo/src/App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>AR Krew!</Text>
+      </View>
+    );
+  }
+}
+export default App;
